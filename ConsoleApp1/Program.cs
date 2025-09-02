2fa71f42f477b52b8bd316bece05797232263fcc
Christopher Pink <christopherbyronpink@yahoo.com>
2025-09-02 15:44:04 -0600

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NewNamesHelper;
+using System.Text;
+
+var namesAndIndiciesList = WriteNameToConsole.WriteNamesToConsole();
+
+foreach (var nameOrIndex in namesAndIndiciesList)
+{
+    Console.WriteLine(nameOrIndex.ToString());
+}
---
+++ NewNamesHelper/WriteNameToConsole.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace NewNamesHelper
+{
+    public class WriteNameToConsole
+    {
+        public static List<string> WriteNamesToConsole()
+        {
+            var namesAndIndiciesList = new List<string>();
+            for (var i = 20; i <= 80; i++)
+            {
+                if (i % 5 == 0 || i % 3 == 0)
+                {
+                    var sb = new StringBuilder();
+                    if (i % 5 == 0)
+                    {
+                        sb.Append("Jeffery ");
+                    }
+                    if (i % 3 == 0)
+                    {
+                        sb.Append("Chris");
+                    }
+
+                    namesAndIndiciesList.Add(sb.ToString());
+                }
+                else
+                {
+                    namesAndIndiciesList.Add($"{i}");
+                }
+            }
+
+            return namesAndIndiciesList;
+        }
+    }
+}
